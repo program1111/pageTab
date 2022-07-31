5544890f858c1b5e2a111e7d775245e804c46ef0
zoey <8101244+a_a123456@user.noreply.gitee.com>
2022-07-31 13:24:39 +0800

    页签的实现

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <el-button @click="go('/a')">/a</el-button>
+    <el-button @click="go('/b')">/b</el-button>
+    <el-button @click="go('/c')">/c</el-button>
+    <el-button @click="go('/d')">/d</el-button>
+    <el-button @click="go('/a?v=123')">/a?v=123</el-button>
+    <el-button @click="go('/b?v=123')">/b?v=123</el-button>
+    <page-tabs :keep-alive-component-instance="keepAliveComponentInstance" />
+    <div ref="keepAliveContainer">
+      <keep-alive>
+        <router-view :key="$route.fullPath" />
+      </keep-alive>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import pageTabs from './components/pageTabs.vue'
+export default {
+  name: 'App',
+  components: {
+    pageTabs
+  },
+  mounted () {
+    if (this.$refs.keepAliveContainer) {
+      this.keepAliveComponentInstance = this.$refs.keepAliveContainer.childNodes[0].__vue__
+    }
+  },
+  data () {
+    return {
+      keepAliveComponentInstance: null
+    }
+  },
+  methods: {
+    go (path) {
+      if (path !== this.$route.fullPath) {
+        this.$router.push(path)
+      }
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +49,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,66 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import Router from 'vue-router'
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import A from './pages/a'
+import B from './pages/b'
+import C from './pages/c'
+import D from './pages/d'
 
 Vue.config.productionTip = false
 
+Vue.use(Router)
+Vue.use(Element)
+
+const router = new Router({
+  routes: [
+    // 这个是空白页面，重新加载当前页面会用到
+    {
+      name: 'blank',
+      path: '/blank'
+    },
+    {
+      path: '',
+      meta: {
+        title: '首页'
+      }
+    },
+    {
+      path: '/a',
+      component: A,
+      meta: {
+        title: 'A页面', // 页面标题
+        canMultipleOpen: true // 支持根据参数不同多开不同页签
+      }
+    },
+    {
+      path: '/b',
+      component: B,
+      meta: {
+        title: 'B页面'
+      }
+    },
+    {
+      path: '/c',
+      component: C,
+      meta: {
+        title: 'C页面'
+      }
+    },
+    {
+      path: '/d',
+      component: D,
+      meta: {
+        title: 'D页面'
+      }
+    }
+  ]
+})
+
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
